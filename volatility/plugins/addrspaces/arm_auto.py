2ed9b7553288d2aa5dffe6ebe8920587c7fd4ed5
Pavel Sviderski <pavel@psviderski.name>
2013-03-17 21:07:42 +0400

    Make ArmAddressSpace a superclass of ArmAutoAddressSpace.

--- volatility/plugins/addrspaces/arm_auto.py
+++ volatility/plugins/addrspaces/arm_auto.py
@@ -1,10 +1,9 @@
-import volatility.plugins.addrspaces.intel as intel
+from volatility.plugins.addrspaces import arm
 
 
-class ArmAutoAddressSpace(intel.JKIA32PagedMemory):
+class ArmAutoAddressSpace(arm.ArmAddressSpace):
     order = 850
     cache = False
     pae = False
     paging_address_space = True
     checkname = 'ArmAutoValidAS'
-
