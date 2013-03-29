67019ea8bdecd647a7cdffcefd51addcf1875663
Pavel Sviderski <pavel@psviderski.name>
2013-03-29 18:20:19 +0400

    Rename the auto_dump_vas command to auto_dump_map.

--- volatility/plugins/linux/auto_dump_map.py
+++ volatility/plugins/linux/auto_dump_map.py
@@ -2,20 +2,22 @@ from volatility import debug, obj, utils
 from volatility.plugins.linux import auto_dtblist
 
 
-class linux_auto_dump_vas(auto_dtblist.linux_auto_dtblist):
-    """Dumps virtual address space of a process"""
+class linux_auto_dump_map(auto_dtblist.linux_auto_dtblist):
+    """Dumps virtual address space of a process to disk"""
 
     def __init__(self, config, *args, **kwargs):
-        super(linux_auto_dump_vas, self).__init__(config, *args, **kwargs)
+        super(linux_auto_dump_map, self).__init__(config, *args, **kwargs)
         self._config.add_option('PROC-DTB', type='int', default=None,
-                                help='Process DTB address')
-        self._config.add_option('VAS-OUTPUT-FILE', default=None,
-                                help='Output file to write VAS to')
+                                help="Process DTB address")
+        self._config.add_option('OUTPUTFILE', short_option='O', default=None,
+                                help="Output file to write a dump of virtual address space to")
+        self._config.add_option('DUMP-DIR', short_option='D', default = None,
+                                help = "Directory to write the output files to")
 
     def calculate(self):
         process_dtb = self._config.PROC_DTB
         if not process_dtb:
-            debug.error("Please specify the DTB address of a process (use option --proc-dtb).")
+            debug.error("Please specify a DTB address of a process (use option --proc-dtb).")
         process_as = utils.load_as(self._config, dtb=process_dtb)
         for vaddr, size in process_as.get_available_pages():
             page = process_as.read(vaddr, size)
@@ -23,9 +25,9 @@ class linux_auto_dump_vas(auto_dtblist.linux_auto_dtblist):
                 yield vaddr, page
 
     def render_text(self, outfd, data):
-        output_file = self._config.VAS_OUTPUT_FILE
+        output_file = self._config.OUTPUTFILE
         if not output_file:
-            debug.error("Please specify the output file to write VAS to (use option --vas-output-file).")
+            debug.error("Please specify an output file (use option --outputfile).")
         self.table_header(outfd, [("Start", "[addrpad]"),
                                   ("End", "[addrpad]")])
         with open(output_file, 'wb') as output_fd:
