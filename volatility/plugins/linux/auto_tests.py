ceec2a279bf16d5b3063e166770b39fb47f38c1f
Pavel Sviderski <pavel@psviderski.name>
2013-04-12 18:39:16 +0400

    Slightly improve unit tests command.

--- volatility/plugins/linux/auto_tests.py
+++ volatility/plugins/linux/auto_tests.py
@@ -1,4 +1,6 @@
 import os
+import pdb
+import sys
 from volatility.plugins.linux import auto as linux_auto
 from volatility.plugins.linux.auto_ksymbol import linux_auto_ksymbol
 
@@ -11,6 +13,11 @@ KSYMTAB_TESTS = {
 
 class linux_auto_tests(linux_auto.AbstractLinuxAutoCommand):
     """Unit tests for auto commands"""
+
+    def assertTrue(self, condition):
+        assert condition
+        sys.stdout.write('.')
+        sys.stdout.flush()
 
     def fileid(self):
         path, filename = os.path.split(self._config.LOCATION)
@@ -26,12 +33,9 @@ class linux_auto_tests(linux_auto.AbstractLinuxAutoCommand):
             symbol_name, symbol_vaddr = test.split()
             symbol_vaddr = int(symbol_vaddr, 16)
             try:
-                assert ksymbol_command.get_symbol(symbol_name) == symbol_vaddr
-                print '.',
+                self.assertTrue(ksymbol_command.get_symbol(symbol_name) == symbol_vaddr)
             except:
-                print "FAIL: {0}".format(symbol_name)
-                raise
-        print
+                pdb.set_trace()
 
     def calculate(self):
         print "###### Start unit tests on {0} ######".format(self.fileid())
